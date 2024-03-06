f727efac3215879e5d204ac62506d630b2cea647
ricky0601 <ricky2425@naver.com>
2024-03-06 17:09:17 +0900

    0306 Main modify

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,51 @@
+import React, { useState } from 'react';
 
 const Main = () => {
+    const [isHovered, setIsHovered] = useState(false);
+
     return <div className="Main">
         <session className="Main_Header">
-            <span className="Main_Header_span">DongGeon's <br/>Portfolio</span>
+                <span
+                    className={`Main_Header_span ${isHovered ? 'hovered' : ''}`}
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                >DongGeon's <br/>Portfolio</span>
         </session>
+        <h2 id="portfolio_list">
+            <span className='hidden'>포트폴리오 목록</span>
+        </h2>
         <session className="Main_Main">
             <div className="Main_Main_wrap">
-                메인 영역
+                <article className='Project_list'>
+                    <div className='Project_thumb_wrap'>
+                        <a className='Project_thumb' href='/'></a>
+                    </div>
+                    <div className='Project_info_wrap'>
+                        <div>프로젝트 이름</div>
+                        <div>프로젝트 기간</div>
+                        <div>사용한 기술</div>
+                    </div>
+                </article>
+                <article className='Project_list'>
+                    <div className='Project_thumb_wrap'>
+                        <a className='Project_thumb' href='/'></a>
+                    </div>
+                    <div className='Project_info_wrap'>
+                        <div>프로젝트 이름</div>
+                        <div>프로젝트 기간</div>
+                        <div>사용한 기술</div>
+                    </div>
+                </article>
+                <article className='Project_list'>
+                    <div className='Project_thumb_wrap'>
+                        <a className='Project_thumb' href='/'></a>
+                    </div>
+                    <div className='Project_info_wrap'>
+                        <div>프로젝트 이름</div>
+                        <div>프로젝트 기간</div>
+                        <div>사용한 기술</div>
+                    </div>
+                </article>
             </div>
         </session>
         <session>로고 영역</session>
